ed8636be41d57e7a5c3b443af998229ac317cfa2
nimnimaor <nimnimaor@gmail.com>
2021-01-15 12:38:30 +0200

    Revert "mkmkmk2"
    This reverts commit 8ab10c27bb633e9378aa860d8ef8a0f635d88969.

--- src/index.js
+++ src/index.js
@@ -2,38 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {Router, Route} from 'react-router-dom';
-import history from './history';
-import {Provider} from 'react-redux';
-import store from './store';
-import SignIn from './Views/SignInView';
-import SignUp from './Views/SignUpView';
-import UpdatePassView from './Views/UpdatePassView';
-import ForgotPasswordView from './Views/ForgotPasswordView';
-import AboutView from './Views/AboutView';
-import ConstractionView from './Views/ConstractionView';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>      
-            <Router history={history}>
-                <div>
-                    <Route exact path="/" component={App}></Route>
-                    <Route path="/Signin" component={SignIn}></Route>
-                    <Route path="/SignUp" component={SignUp}></Route>
-                    <Route path="/UpdatePass" component={UpdatePassView}></Route> 
-                    <Route path="/ForgotPass" component={ForgotPasswordView}></Route>
-                    <Route path="/About" component={AboutView}></Route>
-                    <Route path="/Constraction" component={ConstractionView}></Route>
-                </div>
-            </Router>
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
